0de37d5349d653c72d2f0caea96b999ea6caffe7
yuya0314 <kyotooogaki0314@gmail.com>
2023-07-05 20:49:13 +0900

    Userモデルのnew,show,editを実装

---
+++ app/models/user.rb
@@ -0,0 +1,22 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  
+  validates:name,presence:true
+  validates:self_introduction,length: { maximum: 500 }
+
+  def update_without_current_password(params, *options)
+
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+
+    result = update(params, *options)
+    clean_up_passwords
+    result
+  end
+
+end
