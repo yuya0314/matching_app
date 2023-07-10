e5a1ab750847e99279aab0c7de3b86b86a26cdd7
yuya0314 <kyotooogaki0314@gmail.com>
2023-07-11 08:54:50 +0900

    管理ユーザと管理画面の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  devise_for :admin_users
   get 'event_listings/new'
   devise_for :users,
     controllers: { registrations: 'registrations' } 
