3944a1133b212cb759e20a29e0e0fc8bd3cc7f49
yuya0314 <kyotooogaki0314@gmail.com>
2023-07-20 17:53:06 +0900

    flashメッセージの修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,18 @@ module ApplicationHelper
       "#{page_title} | #{BASE_TITLE}"
       end
   end
+  def bootstrap_class_for(flash_type)
+    case flash_type
+    when "success"
+      "alert-success"
+    when "error"
+      "alert-danger"
+    when "alert"
+      "alert-warning"
+    when "notice"
+      "alert-info"
+    else
+      flash_type.to_s
+    end
+  end
 end
