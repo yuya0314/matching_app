a23df2c0cff31785307317e1df7b4dc5cbf66c1a
yuya0314 <kyotooogaki0314@gmail.com>
2023-07-22 21:12:11 +0900

    deadlineのvalidates追加

--- app/models/event_listing.rb
+++ app/models/event_listing.rb
@@ -5,6 +5,13 @@ class EventListing < ApplicationRecord
   validates :title,presence: true, length: { maximum: 30 }
   validates :deadline,presence: true
   validates :message,presence: true, length: { maximum: 300}
-  validates :capacity,presence: true
-  validates :has_ticket,presence: true
+  validates :capacity,presence: true,numericality: {greater_than: 0}
+  validates :has_ticket,exclusion: {in:[nil]}
+  validate :deadline_not_past
+
+  def deadline_not_past
+    if deadline.present? && deadline < Date.today
+      errors.add(:deadline, "過去の日付は指定できません")
+    end
+  end
 end
