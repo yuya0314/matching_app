b307ec96e25ca540fcfd09f63846916193f95a10
yuya0314 <kyotooogaki0314@gmail.com>
2023-07-23 11:13:32 +0900

    投稿編集機能、削除機能を追加

--- app/controllers/event_listings_controller.rb
+++ app/controllers/event_listings_controller.rb
@@ -14,7 +14,27 @@ class EventListingsController < ApplicationController
       render 'events/show'
     end
   end
-    
+  def edit
+    @event = Event.find(params[:event_id]) 
+    @event_listing = EventListing.find(params[:id])
+  end
+  def update
+    @event_listing = EventListing.find(params[:id])
+    @event = Event.find(params[:event_id])
+    if @event_listing.update(event_listing_params)
+      flash[:success] = '投稿内容を更新しました。'
+      redirect_to [@event_listing.event, @event_listing]
+    else
+      render 'event_listing/edit'
+    end
+  end  
+  def destroy
+    @event_listing = EventListing.find(params[:id])
+    @event_listing.destroy
+    flash[:success] = '投稿を削除しました'
+    redirect_to root_path
+  end
+
   private
 
   def event_listing_params
--- app/models/event_listing.rb
+++ app/models/event_listing.rb
@@ -4,10 +4,10 @@ class EventListing < ApplicationRecord
   has_many :event_registrations
   validates :title,presence: true, length: { maximum: 30 }
   validates :deadline,presence: true
+  validate :deadline_not_past
   validates :message,presence: true, length: { maximum: 300}
   validates :capacity,presence: true,numericality: {greater_than: 0}
   validates :has_ticket,exclusion: {in:[nil]}
-  validate :deadline_not_past
 
   def deadline_not_past
     if deadline.present? && deadline < Date.today
