296d3c377fd06e7ba399e85b1aa8d9fbe9321339
Apple <apple@appurunoMacBook-Pro.local>
2023-08-16 14:31:14 +0900

    マイグレーション削除

--- db/migrate/20230728051557_add_references_to_chat_room_users.rb
+++
@@ -1,6 +0,0 @@
-class AddReferencesToChatRoomUsers < ActiveRecord::Migration[6.1]
-  def change
-    add_reference :chat_room_users, :chat_room, null: false, foreign_key: true
-    add_reference :chat_room_users, :user, null: false, foreign_key: true
-  end
-end
